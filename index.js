f776f3e37b623e8e27055ee0b640df67f4d5539d
unknown <etch.rush@gmail.com>
2023-12-11 15:59:26 -0800

    Edit index.js

--- index.js
+++ index.js
@@ -86,6 +86,12 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
